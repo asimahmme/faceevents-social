3794c23b9071e61c5e49608db32d7a4618fffa5e
Asim Ahmed <asimahmed@asims-mbp.home>
2017-05-03 15:54:08 -0700

    Completed setting up Firebase integration

---
+++ faceevents-social/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  faceevents-social
+//
+//  Created by Asim Ahmed on 5/3/17.
+//  Copyright © 2017 Asim Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
