eb284755dd76acfc761b3cda2ff74253fde7a945
Asim Ahmed <asimahmed@asims-mbp.home>
2017-05-03 20:23:42 -0700

    Sign In UI Custom Classes

---
+++ faceevents-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  faceevents-social
+//
+//  Created by Asim Ahmed on 5/3/17.
+//  Copyright © 2017 Asim Ahmed. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0 / 255.0
